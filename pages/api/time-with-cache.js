254b46c5c161562e8d139b3367b9c1d996d8f142
William Li <email@williamli.dev>
2022-02-22 15:14:26 +0800

    set cache to 3600 (1hr)

--- pages/api/time-with-cache.js
+++ pages/api/time-with-cache.js
@@ -2,6 +2,6 @@
 
 export default (req, res) => {
   res.statusCode = 200
-  res.setHeader('Cache-control', 'public, max-age=0, s-maxage=600, immutable, stale-while-revalidate')
+  res.setHeader('Cache-control', 'public, max-age=0, s-maxage=3600, immutable, stale-while-revalidate')
   res.json({ now: Date.now() })
 }
